290b039ac2796ade4cc367dd35a9e88bc2d6dc40
ubinahn18 <147369590+ubinahn18@users.noreply.github.com>
2024-12-15 22:04:17 +0900

    Update hw_6.py

--- hw_6.py
+++ hw_6.py
@@ -25,10 +25,8 @@ class FCNN(nn.Module):
         super().__init__()
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=48, kernel_size=(3,3), padding=(1,1))
         self.conv2 = nn.Conv2d(in_channels=48, out_channels=96, kernel_size=(3,3), padding=(1,1))
-        self.conv3 = nn.Conv2d(in_channels=96, out_channels=192, kernel_size=(3,3), padding=(1,1))
-        self.conv4 = nn.Conv2d(in_channels=192, out_channels=256, kernel_size=(3,3), padding=(1,1))
         self.pool = nn.MaxPool2d(2,2)
-        self.fc1 = nn.Linear(in_features=8*8*256, out_features=512)
+        self.fc1 = nn.Linear(in_features=8*8*96, out_features=512)
         self.fc2 = nn.Linear(in_features=512, out_features=64)
         self.Dropout = nn.Dropout(0.25)
         self.fc3 = nn.Linear(in_features=64, out_features=10)
@@ -37,10 +35,6 @@ class FCNN(nn.Module):
         x = F.relu(self.conv1(x)) #32*32*48
         x = F.relu(self.conv2(x)) #32*32*96
         x = self.pool(x) #16*16*96
-        x = self.Dropout(x)
-        x = F.relu(self.conv3(x)) #16*16*192
-        x = F.relu(self.conv4(x)) #16*16*256
-        x = self.pool(x) # 8*8*256
         x = self.Dropout(x)
         x = x.view(-1, 8*8*256) # reshape x
         x = F.relu(self.fc1(x))
