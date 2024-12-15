ae6713672f51dd367ab8ad9d842515bfb67e9f00
ubinahn18 <147369590+ubinahn18@users.noreply.github.com>
2024-12-15 13:40:56 +0900

    Update hw_6.py

--- hw_6.py
+++ hw_6.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.optim.lr_scheduler as lr_scheduler
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
 import argparse
 
 
