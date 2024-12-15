5edd84f0168f8eb66ea4c4d86693577c3b50aad0
ubinahn18 <147369590+ubinahn18@users.noreply.github.com>
2024-12-15 21:42:38 +0900

    Update hw_6.py

--- hw_6.py
+++ hw_6.py
@@ -19,30 +19,32 @@ class LinearClassifier(nn.Module):
 
 
 class FCNN(nn.Module):
-    def __init__(self, in_channels: int = 3, hidden_channels: int = 512, out_channels: int = 10):
-        super().__init__()
-        
-        # Convolutional layers
-        self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=3, stride=1, padding=1)  # Output: 32 x 32 x 32
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)  # Reduce dimensions by 2
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)  # Output: 64 x 16 x 16
+     def __init__(self):
+        super(ConvNet, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=48, kernel_size=(3,3), padding=(1,1))
+        self.conv2 = nn.Conv2d(in_channels=48, out_channels=96, kernel_size=(3,3), padding=(1,1))
+        self.conv3 = nn.Conv2d(in_channels=96, out_channels=192, kernel_size=(3,3), padding=(1,1))
+        self.conv4 = nn.Conv2d(in_channels=192, out_channels=256, kernel_size=(3,3), padding=(1,1))
+        self.pool = nn.MaxPool2d(2,2)
+        self.fc1 = nn.Linear(in_features=8*8*256, out_features=512)
+        self.fc2 = nn.Linear(in_features=512, out_features=64)
+        self.Dropout = nn.Dropout(0.25)
+        self.fc3 = nn.Linear(in_features=64, out_features=10)
 
-        # Fully connected layers
-        self.fc1 = nn.Linear(64 * 16 * 16, hidden_channels)  # Flattened size after conv and pooling
-        self.fc2 = nn.Linear(hidden_channels, out_channels)
-        
-    def forward(self, x: torch.Tensor):
-        # Apply convolutional layers with ReLU and MaxPool
-        x = self.pool(torch.relu(self.conv1(x)))
-        x = self.pool(torch.relu(self.conv2(x)))
-        
-        # Flatten the output of the last conv layer for the fully connected layers
-        x = x.view(x.size(0), -1)
-        
-        # Fully connected layers
-        x = torch.relu(self.fc1(x))
-        x = self.fc2(x)
-        
+    def forward(self, x):
+        x = F.relu(self.conv1(x)) #32*32*48
+        x = F.relu(self.conv2(x)) #32*32*96
+        x = self.pool(x) #16*16*96
+        x = self.Dropout(x)
+        x = F.relu(self.conv3(x)) #16*16*192
+        x = F.relu(self.conv4(x)) #16*16*256
+        x = self.pool(x) # 8*8*256
+        x = self.Dropout(x)
+        x = x.view(-1, 8*8*256) # reshape x
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.Dropout(x)
+        x = self.fc3(x)
         return x
 
 
